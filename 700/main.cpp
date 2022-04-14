df13a09c6d56444126250ef724960362feb43615
terminaldweller <thabogre@gmail.com>
2022-04-14 09:11:01 +0430

    update

---
+++ 700/main.cpp
@@ -0,0 +1,30 @@
+
+#include "header.hpp"
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  TreeNode *searchBST(TreeNode *root, int val) {
+    while (root) {
+      if (root->val > val) {
+        root = root->left;
+      } else if (root->val < val) {
+        root = root->right;
+      } else {
+        return root;
+      }
+    }
+    return nullptr;
+  }
+};
+
+int main(int argc, char **argv) { return 0; }
