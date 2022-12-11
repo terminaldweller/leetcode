47e6ccd958e5d9e83a474fd2763e243311d2f516
terminaldweller <thabogre@gmail.com>
2022-12-11 10:48:28 +0330

    124

---
+++ 124/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func walk(node *TreeNode, sum *int) int {
+	if node == nil {
+		return 0
+	}
+
+	left := max(0, walk(node.Left, sum))
+	right := max(0, walk(node.Right, sum))
+	*sum = max(*sum, left+right+node.Val)
+
+	return max(left, right) + node.Val
+}
+
+func maxPathSum(root *TreeNode) int {
+	sum := math.MinInt
+
+	walk(root, &sum)
+
+	return sum
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
